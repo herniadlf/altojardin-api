eaa9cc93a6f4a123e3e9a6ceea1370813fdc71fd
Hernan de la Fuente <hher.10@gmail.com>
2019-11-11 11:10:07 -0300

    03 - telegram id invalid

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,7 +4,7 @@ require_relative '../../app/repositories/order_repository'
 DeliveryApi::App.controllers :order do
   post '/', provides: :json do
     user = UserRepository.new.find_by_telegram_id(params['telegram_id'])
-    params[:user_id] = user.id
+    params[:user_id] = user.id unless user.nil?
     order = Order.new(params)
     result = OrderRepository.new.save(order)
     return { 'message': { 'order_number': order.id } }.to_json if result
