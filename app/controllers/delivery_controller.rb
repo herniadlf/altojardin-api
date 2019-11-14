d6484f300c50f0ecd9a0a0bb700d4495fc47cd49
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-11-14 14:34:53 -0300

    02 Agrego el mensaje de error a la api

--- app/controllers/delivery_controller.rb
+++ app/controllers/delivery_controller.rb
@@ -6,9 +6,11 @@ DeliveryApi::App.controllers :delivery do
     delivery = Delivery.new(params)
     return { 'delivery_id': delivery.id }.to_json if DeliveryRepository.new.save(delivery)
 
+    key = delivery.errors.messages[delivery.errors.messages.keys.first][0]
     status 400
     {
-      'error': delivery.errors.messages[delivery.errors.messages.keys.first][0]
+      'error': key,
+      'message': Messages.new.get_message(key)
     }.to_json
   end
 end
