c5fd2a2c55e4137e82c8193a99f0ff1dca0bcc26
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-11-17 10:57:26 -0300

    08 - Refactor tests

--- spec/repositories/delivery_repository_spec.rb
+++ spec/repositories/delivery_repository_spec.rb
@@ -4,37 +4,48 @@ require_relative '../../app/models/delivery'
 describe DeliveryRepository do
   let(:repository) { described_class.new }
 
-  let(:new_delivery) do
-    new_delivery = Delivery.new(
-      username: 'delivery'
+  let(:pepebicicleta_delivery) do
+    pepebicicleta_delivery = Delivery.new(
+      username: 'pepebicicleta'
     )
-    repository.save(new_delivery)
-    new_delivery
+    pepebicicleta_delivery.available = false
+    repository.save(pepebicicleta_delivery)
+    pepebicicleta_delivery
+  end
+
+  let(:juanmotoneta_delivery) do
+    juanmotoneta_delivery = Delivery.new(
+      username: 'juanmotoneta'
+    )
+    juanmotoneta_delivery.available = false
+    repository.save(juanmotoneta_delivery)
+    juanmotoneta_delivery
   end
 
   it 'should find delivery' do
-    id = new_delivery.id
-    delivery = repository.find(id)
-    expect(delivery.user_id).to eq id
+    id = pepebicicleta_delivery.id
+    pepebicicleta_delivery = repository.find(id)
+    expect(pepebicicleta_delivery.user_id).to eq id
   end
 
   it 'should persist available delivery' do
-    expect(repository.find(new_delivery.id).available).to eq true
-    new_delivery.available = false
-    repository.save(new_delivery)
-    expect(repository.find(new_delivery.id).available).to eq false
+    expect(repository.find(pepebicicleta_delivery.id).available).to eq false
+    pepebicicleta_delivery.available = true
+    repository.save(pepebicicleta_delivery)
+    expect(repository.find(pepebicicleta_delivery.id).available).to eq true
   end
 
-  it 'should find available deliveries' do
-    expect(new_delivery.available).to eq true
-    delivery = repository.find_first_available
-    expect(delivery.nil?).to eq false
-  end
+  context 'when deliveries have no orders' do
+    it 'should find available deliveries' do
+      pepebicicleta_delivery.available = true
+      repository.save(pepebicicleta_delivery)
+      pepebicicleta_delivery = repository.find_first_available
+      expect(pepebicicleta_delivery.nil?).to eq false
+    end
 
-  it 'should not find available deliveries' do
-    new_delivery.available = false
-    repository.save(new_delivery)
-    delivery = repository.find_first_available
-    expect(delivery.nil?).to eq true
+    it 'should not find available deliveries' do
+      pepebicicleta_delivery = repository.find_first_available
+      expect(pepebicicleta_delivery.nil?).to eq true
+    end
   end
 end
