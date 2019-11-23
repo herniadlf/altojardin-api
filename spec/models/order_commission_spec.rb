5486ee2e35cfb5b68ff407c8ff43704b160b9d8b
Hernan de la Fuente <hher.10@gmail.com>
2019-11-23 09:07:08 -0300

    07 - add menu_familiar test

--- spec/models/order_commission_spec.rb
+++ spec/models/order_commission_spec.rb
@@ -8,14 +8,19 @@ describe OrderCommission do
   end
 
   describe 'No rainy day commission' do
-    it 'should have be 5.0 when order is menu_individual and rating is 3' do
+    it 'should be 5.0 when order is menu_individual and rating is 3' do
       commission = described_class.new(order_price: 100.0, rating: 3, rainy: false)
       expect(commission.calculate).to eq 5.0
     end
 
-    it 'should have be 8.75 when order is menu_parejas and rating is 3' do
+    it 'should be 8.75 when order is menu_parejas and rating is 3' do
       commission = described_class.new(order_price: 175.0, rating: 3, rainy: false)
       expect(commission.calculate).to eq 8.75
+    end
+
+    it 'should be 12.5 when order is menu_familiar and rating is 3' do
+      commission = described_class.new(order_price: 250.0, rating: 3, rainy: false)
+      expect(commission.calculate).to eq 12.5
     end
   end
 end
