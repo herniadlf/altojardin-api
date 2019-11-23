b30c3d67a5335eed0900cb4fe27b3c1fe80a97a0
Hernan de la Fuente <hher.10@gmail.com>
2019-11-23 09:16:29 -0300

    07 - completed COM1 with controller

--- app/controllers/commission_controller.rb
+++ app/controllers/commission_controller.rb
@@ -1,5 +1,11 @@
+require_relative '../repositories/order_repository'
+require_relative '../models/order_commission'
+
 DeliveryApi::App.controllers do
   get '/commission/:order_id', provides: :json do
-    status 200
+    order = OrderRepository.new.find! params[:order_id]
+    data = { order_price: order.price, rating: order.rating, rainy: false }
+    commission = OrderCommission.new(data).calculate
+    { commission_amount: commission }.to_json
   end
 end
