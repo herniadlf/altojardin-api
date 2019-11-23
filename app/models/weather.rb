2e874eca80d5b3fca2001b98ddfd99eb55c33b02
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-11-23 17:34:31 -0300

    10 agrego id a weather

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,7 +1,8 @@
 class Weather
-  attr_accessor :date, :rain
+  attr_accessor :id, :date, :rain
 
   def initialize(data)
+    @id = data[:id]
     @date = data[:date]
     @rain = data[:rain]
   end
--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Weather do
-  describe described_class.new(date: '2019-6-6', rain: false) do
+  describe described_class.new(id: 1, date: '2019-6-6', rain: false) do
+    it { is_expected.to respond_to(:id) }
     it { is_expected.to respond_to(:date) }
     it { is_expected.to respond_to(:rain) }
   end
