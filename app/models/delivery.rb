adb2b5430266b9f703b21c97cadeba7e8f2d0faa
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-11-29 15:46:40 -0300

    14 - Añado atributo para saber la cantiada de ordenes que hizo un delivery

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,7 +1,7 @@
 require_relative '../../app/models/user'
 
 class Delivery < User
-  attr_accessor :user_id, :available, :occupied_quantity
+  attr_accessor :user_id, :available, :occupied_quantity, :orders_done_today
 
   CAPACITY = 4
 
@@ -10,5 +10,6 @@ class Delivery < User
     @user_id = data[:user_id]
     @available = data[:available].nil? ? true : data[:available]
     @occupied_quantity = data[:occupied_quantity]
+    @orders_done_today = data[:orders_done_today]
   end
 end
--- spec/models/delivery_spec.rb
+++ spec/models/delivery_spec.rb
@@ -5,5 +5,6 @@ describe Delivery do
     it { is_expected.to respond_to(:user_id) }
     it { is_expected.to respond_to(:available) }
     it { is_expected.to respond_to(:occupied_quantity) }
+    it { is_expected.to respond_to(:orders_done_today) }
   end
 end
