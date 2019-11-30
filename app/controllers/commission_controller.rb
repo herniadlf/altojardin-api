7ac627fa3648dac242804d75dd025c6d7a41ab88
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-11-30 16:51:56 -0300

    Fix calculo de comision

--- app/controllers/commission_controller.rb
+++ app/controllers/commission_controller.rb
@@ -5,9 +5,10 @@ require_relative '../../app/exceptions/commission_exception'
 DeliveryApi::App.controllers do
   get '/commission/:order_id', provides: :json do
     order = OrderRepository.new.find! params[:order_id]
-    data = { order_price: order.price, rating: order.rating, status: order.status }
+    data = { order_price: order.price, rating: order.rating, status: order.status.id }
+    puts(data.inspect)
     commission = OrderCommission.new(data).calculate
-    { commission_amount: commission }.to_json
+    return { commission_amount: commission }.to_json
   rescue CommissionException => e
     error_response(e.key, 400)
   end
