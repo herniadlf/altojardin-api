c219bcfa8f1565fdd0ae8375038ff790811277c7
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-11-29 23:22:59 -0300

    13 - Verifico que no se devuelven ordenes no historicas

--- spec/repositories/order_repository_spec.rb
+++ spec/repositories/order_repository_spec.rb
@@ -117,6 +117,14 @@ describe OrderRepository do
     expect(result.first.id).to eq order.id
   end
 
+  it 'should not find no historic orders of client' do
+    order = Order.new(user_id: order_owner.id, menu: 'menu_individual',
+                      status: received_status)
+    repository.save(order)
+    result = repository.find_historic_by_client_username(order_owner.username)
+    expect(result.length).to eq 0
+  end
+
   it 'should be rated' do
     order = Order.new(user_id: order_owner.id, menu: 'menu_individual',
                       status: OrderStatusDelivered.new, assigned_to: delivery.id)
